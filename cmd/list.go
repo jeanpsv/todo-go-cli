eb4d5048d883ea36c01d8ee15072bb5a852f8a96
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2024-08-20 17:27:23 -0300

    implements list and add commands with simple file storage

--- cmd/list.go
+++ cmd/list.go
@@ -22,46 +22,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new todo",
-	Long: `Add will create a new todo item to the list:
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: addRun,
+	Run: func(cmd *cobra.Command, args []string) {
+		items, err := todo.ReadItems(".todos.json")
+
+		if err != nil {
+			fmt.Printf("%v", err)
+		}
+
+		fmt.Println(items)
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func addRun(cmd *cobra.Command, args []string) {
-	items, err := todo.ReadItems(".todos.json")
-
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-
-	for _, x := range args {
-		items = append(items, todo.Item{Text: x})
-	}
-
-	err = todo.SaveItems(".todos.json", items)
-
-	if err != nil {
-		fmt.Errorf("%v", err)
-	}
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
